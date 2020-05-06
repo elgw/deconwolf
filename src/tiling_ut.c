bc788059be83ef29dc40f59b259a50bd8c3c54ba
elgw <erik.wernersson@gmail.com>
2020-05-06 15:45:54 +0200

    issue discovered

--- src/tiling_ut.c
+++ src/tiling_ut.c
@@ -10,23 +10,32 @@ int main(int argc, char ** argv)
   int overlap = 2;
   int maxSize = 400;
 
+  if(argc == 6)
+  {
+    M = atol(argv[1]);
+    N = atol(argv[2]);
+    P = atol(argv[3]);
+    maxSize = atol(argv[4]);
+    overlap = atol(argv[5]);
+  }
+
   tiling * T = tiling_create(M,N,P, maxSize, overlap);
   tiling_show(T);
 
-  float w = tiling_getWeights(T, 512, 512, 0);
-  printf("w = %f\n", w);
-
+  printf("Generating weight map\n"); fflush(stdout);
   float * W = malloc(M*N*sizeof(float));
   for(int aa = 0; aa<M; aa++)
   {
     for(int bb = 0; bb<N; bb++)
     {
-      W[aa + bb*M] = tiling_getWeights(T, aa, bb, 0);
+      float w = tiling_getWeights(T, aa, bb, 0);
+      assert(w>0);
+      W[aa + bb*M] = w;
     }
   }
 
   char outFile[] = "tiling_ut_weights.tif";
-  printf("Writing weights to %s\n", outFile);
+  printf("Writing weights to %s\n", outFile); fflush(stdout);
   writetif(outFile, W, M, N, P);
   free(W);
 
