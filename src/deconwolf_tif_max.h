f17070c70de292e6268b8d327d5485218062032b
Erik Wernersson <erik.wernersson@gmail.com>
2021-03-09 13:00:35 +0100

    added missing file

---
+++ src/deconwolf_tif_max.h
@@ -0,0 +1,14 @@
+#ifndef _deconwolf_tif_max_h_
+#define _deconwolf_tif_max_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include "fim.h"
+#include "fim_tiff.h"
+#include "dw_version.h"
+
+int dw_tiff_max(int argc, char ** argv);
+
+#endif
