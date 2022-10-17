4755f0c0fa12008b24f9d34666bc7e31eb0ceb6d
Erik Wernersson <erik.wernersson@gmail.com>
2022-10-18 00:03:10 +0200

    shbcl2 preparing for final gpu transition

--- src/kernels/cl_shb_update.c
+++ src/kernels/cl_shb_update.c
@@ -2,7 +2,7 @@ __kernel void cl_shb_update(__global float * P,
                             const __global float * x,
                             const __global float * xp,
                             const __global size_t * real_size,
-                            const __global float alpha)
+                            const __global float * alpha)
 {
     const size_t idx = get_global_id(0);
 
