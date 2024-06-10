1f3a2b987dedd3b38b732ef6cf8dd6ea76c39ef3
Erik Wernersson <erik.wernersson@gmail.com>
2024-06-10 15:22:57 +0200

    v0.4.1 -> v0.4.2

--- src/dw_version.h
+++ src/dw_version.h
@@ -2,7 +2,7 @@
 
 #define DW_VERSION_MAJOR "0"
 #define DW_VERSION_MINOR "4"
-#define DW_VERSION_PATCH "1"
+#define DW_VERSION_PATCH "2"
 #define deconwolf_version DW_VERSION_MAJOR "." \
     DW_VERSION_MINOR "." \
     DW_VERSION_PATCH
