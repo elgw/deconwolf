d0c83d84e0b2c14bad93fd772f4993e051f05340
elgw <erikw@erikwfractal>
2025-01-21 12:42:10 +0100

    ...

---
+++ doc/gh-pages/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'deconwolf'
+copyright = '2023-2024, Erik Wernersson'
+author = 'Erik Wernersson'
+release = '0.4.4'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+# https://github.com/readthedocs/sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
---
+++ doc/gh-pages/source/index.rst
@@ -0,0 +1,22 @@
+deconwolf v0.4.3
+================
+
+.. hint::
+
+   The source code can be found on `github <https://github.com/elgw/deconwolf>`__.
+
+..
+   And there is also `deconwolf.fht.org <https://deconwolf.fht.org>`__.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents
+
+   start
+   install
+   usage
+   spots
+   shifts
+   segmentation
+   history
+   license
