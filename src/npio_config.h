2a4d8df7014080f8dc75e9029bf1fabb2bb4c585
Erik Wernersson <erik.wernersson@gmail.com>
2025-03-11 12:27:40 +0100

    for npio

---
+++ src/npio_config.h
@@ -0,0 +1,7 @@
+// cmake generates npio_config.h from npio_config.h.in
+#define NPIO_VERSION_MAJOR "0"
+#define NPIO_VERSION_MINOR "0"
+#define NPIO_VERSION_PATCH "10"
+#define NPIO_VERSION NPIO_VERSION_MAJOR "."     \
+    NPIO_VERSION_MINOR "."                      \
+    NPIO_VERSION_PATCH
