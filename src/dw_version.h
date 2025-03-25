f5bc77c6d6855ec4c69091bbf89ad9adf703769f
Erik Wernersson <erik.wernersson@gmail.com>
2025-03-25 10:40:30 +0100

    set to new version

--- src/dw_version.h
+++ src/dw_version.h
@@ -4,7 +4,7 @@
 
 #define DW_VERSION_MAJOR "0"
 #define DW_VERSION_MINOR "4"
-#define DW_VERSION_PATCH "5"
+#define DW_VERSION_PATCH "6"
 #define deconwolf_version DW_VERSION_MAJOR "." \
     DW_VERSION_MINOR "." \
     DW_VERSION_PATCH
