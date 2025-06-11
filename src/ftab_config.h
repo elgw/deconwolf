9a5ddd59390029f60967596c629e7cda61ec96dd
Erik Wernersson <erik.wernersson@gmail.com>
2025-06-11 13:15:00 +0200

    0.1.5

---
+++ src/ftab_config.h
@@ -0,0 +1,8 @@
+// cmake generates ftab_config.h from ftab_config.h.in
+#define FTAB_VERSION_MAJOR "0"
+#define FTAB_VERSION_MINOR "1"
+#define FTAB_VERSION_PATCH "5"
+#define FTAB_VERSION FTAB_VERSION_MAJOR "."     \
+    FTAB_VERSION_MINOR "."                      \
+    FTAB_VERSION_PATCH
+
