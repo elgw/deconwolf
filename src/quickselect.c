df3c2ae26268252017da779c31a29a75f59dd87d
Erik Wernersson <erik.wernersson@gmail.com>
2026-09-10 00:10:08 +0200

    Updated to v 1.0.3

--- src/quickselect.c
+++ src/quickselect.c
@@ -28,6 +28,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+typedef int64_t i64;
 
 #ifndef QUICKSELECT_F32
 #ifndef QUICKSELECT_F64
@@ -41,22 +44,19 @@ typedef float etype;
 #endif
 
 #ifdef QUICKSELECT_F64
-
 #ifdef QUICKSELECT_F32
 #error "both QUICKSELECT_F32 and QUICKSELECT_F64 are defined"
 #endif
-
 typedef double etype;
 #define FNAME(x) x ## _f64
-
 #endif
 
 #ifndef FNAME
 #error "quickselect precision not defined, use -DQUICKSELECT_FP32 or -DQUICKSELECT_FP64"
 #endif
 
-#define qs_max(x,y) (x>y ? x : y)
-#define qs_min(x,y) (x<y ? x : y)
+#define qs_max(x,y) ((x>y) ? x : y)
+#define qs_min(x,y) ((x<y) ? x : y)
 
 static etype
 med3(etype a, etype b, etype c)
@@ -64,13 +64,34 @@ med3(etype a, etype b, etype c)
     return qs_max(qs_min(a,b),qs_min(c,qs_max(a,b)));
 }
 
-static etype
-med5(etype a, etype b, etype c, etype d, etype e)
+static inline etype
+med5(const etype * X)
 {
-    etype f=qs_max(qs_min(a,b),qs_min(c,d)); // discards lowest from first 4
-    etype g=qs_min(qs_max(a,b),qs_max(c,d)); // discards biggest from first 4
-    return qs_max(qs_min(e,f),qs_min(g,qs_max(e,f))); /* median of 3 elements */
+    etype f=qs_max(qs_min(X[0],X[1]),qs_min(X[2],X[3])); // discards lowest from first 4
+    etype g=qs_min(qs_max(X[0],X[0]),qs_max(X[2],X[3])); // discards biggest from first 4
+    return qs_max(qs_min(X[4],f),qs_min(g,qs_max(X[4],f))); /* median of 3 elements */
 }
+
+static inline etype
+min5(const etype * X)
+{
+    etype min = X[0];
+    for(int kk = 1; kk < 5; kk++){
+        X[kk] < min ? min = X[kk] : 0;
+    }
+    return min;
+}
+
+static inline etype
+max5(const etype * X)
+{
+    etype max = X[0];
+    for(int kk = 1; kk < 5; kk++){
+        X[kk] > max ? max = X[kk] : 0;
+    }
+    return max;
+}
+
 
 static void
 array_minmax(const etype * restrict A, const size_t N,
@@ -78,57 +99,27 @@ array_minmax(const etype * restrict A, const size_t N,
 {
     *min = A[0];
     *max = A[0];
-    for(size_t kk = 1; kk < N; kk++)
-    {
-        *min > A[0] ? *min = A[0] : 0;
-        *max < A[0] ? *max = A[0] : 0;
+    for(size_t kk = 1; kk < N; kk++) {
+        *min > A[kk] ? *min = A[kk] : 0;
+        *max < A[kk] ? *max = A[kk] : 0;
     }
     return;
 }
 
-static etype
-_quickselect(etype * restrict X, const size_t N, const size_t s,
-             const int use_pb, etype ** restrict PB);
-
-static etype get_pivot_random(const etype * X, size_t N)
+#if 0
+#define SWAPE(a, b) etype t = a; a = b; b = t;
+static void sort4(etype X[4])
 {
-    size_t idx = rand() % N;
-    return X[idx];
+    if(X[0] > X[2]){SWAPE(X[0], X[2])};
+    if(X[1] > X[3]){SWAPE(X[1], X[3])};
+    if(X[0] > X[1]){SWAPE(X[0], X[1])};
+    if(X[2] > X[3]){SWAPE(X[2], X[3])};
+    if(X[1] > X[2]){SWAPE(X[1], X[2])};
 }
+#endif
 
 static etype
-get_pivot(const etype * restrict X, const size_t N, etype ** restrict PB)
-{
-    const int use_pb = 1;
-    assert(N > 1);
-    if(N == 2)
-    {
-        return X[0];
-    }
-
-    if(N < 5)
-    {
-        return med3(X[0], X[(N-1)/2], X[N-1]);
-    }
-
-    size_t nM = N / 5;
-
-
-    etype * M = PB[0];
-    PB[0] += nM; /* Next time, use a higher address */
-    for(size_t mm = 0; mm < nM; mm++)
-    {
-        const etype * P = X+5*mm;
-        M[mm] = med5(P[0], P[1], P[2], P[3], P[4]);
-    }
-
-    etype pivot = _quickselect(M, nM, nM/2, use_pb, PB);
-
-    PB[0] -= nM; /* Done, return the data */
-
-    return pivot;
-}
-
+_quickselect(etype * restrict X, const size_t N, const size_t s);
 
 static void
 partition(etype * restrict X, const size_t n,
@@ -136,197 +127,201 @@ partition(etype * restrict X, const size_t n,
           size_t * nLow, size_t * nHigh)
 {
     assert(n > 0);
-    int64_t low = -1;
-    int64_t high = n;
-    int64_t n2 = n;
+    i64 low = -1;
+    i64 high = (i64) n;
+    i64 n2 = (i64) n;
 
-    while(1)
-    {
-        do
-        {
+    while(1){
+        do {
             low++;
         } while ( low < n2 && X[low] <= pivot );
 
-        do
-        {
+        do {
             high--;
         } while ( high > 0 && X[high] > pivot );
 
-        if(low >= high)
-        {
-            *nLow = low;
-            *nHigh = n-*nLow;
-#ifndef NDEBUG
-            assert(low >= 0);
-            assert(high < (int64_t) n);
-            assert(*nLow + *nHigh == n );
-            for(int64_t kk = 0; kk < low; kk++)
-            {
-                assert(X[kk] <= pivot);
-            }
-            for(int64_t kk = low; kk < n2; kk++)
-            {
-                assert(X[kk] > pivot);
-            }
-            assert(high > -1);
-#endif
-            return;
-        } else {
-            // Make a swap
-#ifndef NDEBUG
-            assert(low >= 0);
-            assert(high < (int64_t) n);
+        if(low >= high) {
+            goto done;
+        } else { // swap
             assert(low < high);
-#endif
             etype t = X[low];
             X[low] = X[high];
             X[high] = t;
         }
     }
 
+done:
+    *nLow = (size_t) low;
+    *nHigh = n - (size_t) low;
+
+#ifdef QUICKSELECT_DEBUG
+    assert(low >= 0);
+    assert(high < (i64) n);
+    assert(*nLow + *nHigh == n );
+    for(i64 kk = 0; kk < low; kk++) {
+        assert(X[kk] <= pivot);
+    }
+    for(i64 kk = low; kk < n2; kk++) {
+        assert(X[kk] > pivot);
+    }
+    assert(high > -1);
+#endif
     return;
 }
 
-static etype
-_quickselect(etype * restrict X, const size_t N, const size_t s,
-             const int use_pb, etype ** restrict PB)
-{
 
+// Returns 1 if a pivot could be found
+// or 0 if all elements are equal, which means that we found our number
+static int
+get_pivot(const etype * X, size_t N, etype * pivot)
+{
+    // Strategy 1
+    // Something more dynamic would be compelling.
+    // please benchmark on much data!
+    etype vals[5];
+    if(N < 1024){ // median of 5 maximally spread numbers
+        for(size_t kk = 0; kk < 5; kk++){
+            vals[kk] = X[kk*(N-1)/4];
+        }
+    } else { // med5(med5, med5, ...)
+        for(size_t kk = 0; kk < 5; kk++){
+            etype lvals[5];
+            for(size_t ll = 0; ll < 5; ll++){
+                size_t ii = kk*5 + ll;
+                lvals[ll] = X[ii*(N-1)/25];
+            }
+            vals[kk] = med5(lvals);
+        }
+    }
+
+    *pivot = med5(vals);
+
+    if(! (*pivot == min5(vals) || *pivot == max5(vals))){
+        return 0;
+    }
+
+#if 0
+    // Strategy 2: median of 5 values from random locations
+    // Probably better to do kk*p % N, where gcd(p, N) == 1
+    // and p large
+    if(N > 100)
+    {
+        for(int kk = 0; kk < 5; kk++){
+            vals[0] = X[(size_t) rand() % N];
+        }
+
+        *pivot = med5(vals);
+        if(! (*pivot == min5(vals) || *pivot == max5(vals))){
+            return 0;
+        }
+    }
+#endif
+    // Last resort, check if all numbers are equal
+    etype min_value, max_value;
+    array_minmax(X, N, &min_value, &max_value);
+    if(min_value == max_value) {
+        *pivot = max_value;
+        return 1; // all equal and we are done
+    }
+    // If not all equal, select something in-between
+    *pivot = (min_value + max_value) / (etype) 2.0;
+    // there might not by any value between them
+    // nextafter(min_value, max_value) == max_value
+    // so we simply rely on this
+    if(*pivot >= max_value || *pivot <= min_value){
+        *pivot = min_value;
+    }
+    return 0;
+}
+
+static etype
+_quickselect(etype * restrict X, const size_t N, const size_t s)
+{
     assert( s < N );
     assert( N != 0 );
     /* Only one element left. Has to be the one we are looking for */
-    if(N == 1)
-    {
-        assert(s == 0);
+    if(N == 1){
         return(X[0]);
     }
 
-    if(N == 2)
-    {
-        if(s == 0)
-        {
+    if(N == 2){
+        if(s == 0) {
             return qs_min(X[0], X[1]);
         } else {
             return qs_max(X[0], X[1]);
         }
     }
 
-    if(N == 3)
-    {
-        if(s == 0)
-        {
+    if(N == 3) {
+        if(s == 0) {
             return qs_min(qs_min(X[0], X[1]), X[2]);
         }
-        if(s == 1)
-        {
+        if(s == 1) {
             return med3(X[0], X[1], X[2]);
         }
         return qs_max(qs_max(X[0], X[1]), X[2]);
     }
 
-    etype pivot;
-    if(use_pb) {
-        pivot = get_pivot(X, N, PB);
-    } else {
-        pivot = med5(X[0], X[(N-1)/4],
-                     X[(N-1)/2], X[3*(N-1)/4], X[N-1]);
+#if 0 // would make it slower
+    if(N == 4){
+        sort4(X);
+        return X[s];
+    }
+#endif
+
+    etype pivot = X[0];
+    if(get_pivot(X, N, &pivot)){
+        // All elements equal, no pivot could be found
+        return pivot;
     }
 
-    /* Solve the dutch flag problem,
-     *  split into
-     *  A: Lower than the pivot,
-     *  B: equal to the pivot
-     *  C Higher than the pivot
-     * However we skip the B partition ...
-     */
-
+    //  split into
+    //  A: Lower than the pivot,
+    //  C: Higher or equal to the pivot
     size_t nA=0, nC=0;
     partition(X, N, pivot, &nA, &nC);
-
-    /* Avoid infinite recursion */
-    if(nA == 0 || nC == 0)
-    {
-        pivot = get_pivot_random(X, N);
-        partition(X, N, pivot, &nA, &nC);
+    if(nA + nC != N){
+        fprintf(stderr, "Internal error, L%d\n", __LINE__);
+        return 0;
     }
 
-    /* Possibly constant array, fallback */
-    if(nA == 0 || nC == 0)
-    {
-        etype min_value, max_value;
-        array_minmax(X, N, &min_value, &max_value);
-
-        if(min_value == max_value)
-        {
-            return min_value;
-        } else {
-            pivot = (min_value + max_value) / 2;
-            partition(X, N, pivot, &nA, &nC);
-        }
+    if(nA == 0){
+        fprintf(stderr, "Internal error, L%d\n", __LINE__);
+        return 0;
     }
-
-    assert(nA != 0);
-    assert(nC != 0);
-    assert(nA + nC == N);
+    if(nC == 0){
+        fprintf(stderr, "Internal error, L%d\n", __LINE__);
+        return 0;
+    }
 
     etype *A = X;
     etype *C = X+nA;
 
-#ifndef NDEBUG
+#ifdef QUICKSELECT_DEBUG
     for(size_t kk = 0; kk<nA; kk++)
     { assert(A[kk] <= pivot); }
     for(size_t kk = 0; kk<nC; kk++)
     { assert(C[kk] > pivot); }
 #endif
 
-    /* Recurse on the partition that can contain the median */
-
-    if(s < nA)
-    {
-        return _quickselect(A, nA, s, use_pb, PB);
+    // Recurse on the partition that can contain element with index s
+    if(s < nA) {
+        return _quickselect(A, nA, s);
+    } else {
+        return _quickselect(C, nC, s-nA);
     }
-
-    return _quickselect(C, nC, s-nA, use_pb, PB);
 }
 
-
-static etype
-quickselect_start(const etype * X, size_t N, size_t s, int use_pb)
+etype
+FNAME(qselect)(const etype * X, size_t N, size_t s)
 {
-    assert(s < N);
+    if(X == NULL){ return 0; }
+    if(s >= N){ return 0; }
 
-    if(N == 1)
-    {
-        assert(s == 0);
-        return(X[0]);
-    }
-
-    etype * X2 = calloc(N, sizeof(etype));
+    etype * X2 = malloc(N*sizeof(etype));
     assert(X2 != NULL);
     memcpy(X2, X, N*sizeof(etype));
-
-
-    etype * PB = NULL;
-    if(use_pb) {
-        PB = calloc(N/2, sizeof(etype)); /* Pivot buffer */
-        assert(PB != NULL);
-    }
-
-    etype element = _quickselect(X2, N, s, use_pb, &PB);
-
-    if(use_pb){
-        free(PB);
-    }
-
+    etype element = _quickselect(X2, N, s);
     free(X2);
     return element;
-}
-
-etype FNAME(qselect)(const etype * X, size_t N, size_t s)
-{
-    if( X == NULL )
-    {
-        return 0;
-    }
-    return quickselect_start(X, N, s, 0);
 }
--- src/quickselect.h
+++ src/quickselect.h
@@ -1,56 +1,73 @@
 #pragma once
-#include <stddef.h>
-
-/*
-  Copyright 2025 Erik Wernersson
-
-  Permission is hereby granted, free of charge, to any person
-  obtaining a copy of this software and associated documentation files
-  (the “Software”), to deal in the Software without restriction,
-  including without limitation the rights to use, copy, modify, merge,
-  publish, distribute, sublicense, and/or sell copies of the Software,
-  and to permit persons to whom the Software is furnished to do so,
-  subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-*/
-
-/*
-  An implementation of the quicksort algorithm.
-
-  Only one of the functions declared below will be built at a time,
-  the default is for double precision. For single precision compile
-  with -DQUICKSELECT_F32
-
-  Original repository id: https://github.com/elgw/quickselect
-  based on arch/24/03/11 (private)
-
-  CHANGELOG
-
-  1.0.1 renames min and max macros to qs_min and qs_max since min and
-  max are already defined on some compilers/standard libraries.
-*/
+// Copyright 2025 Erik Wernersson
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation files
+// (the “Software”), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 #define ELGW_QS_VERSION_MAJOR 1
 #define ELGW_QS_VERSION_MINOR 0
-#define ELGW_QS_VERSION_PATCH 1
+#define ELGW_QS_VERSION_PATCH 3
 
-/** @brief quickselect
- * @param X the data points
- * @param nX number of elements of X
- * @param s what element to select from sorted X
- * @return the value of element s of sorted X.
- */
+// An implementation of the quicksort algorithm.
+//
+// Only one of the functions declared below will be built at a time,
+// the default is for double precision. For single precision compile
+// with -DQUICKSELECT_F32
+//
+// Version history
+//
+// 1.0.3
+// - Fix: pivot finding would cause infinite loops when
+//   the min and max values were very similar.
+//
+// 1.0.2
+// - Fix: bug in array_minmax
+// - better test suite
+//
+// 1.0.1
+// - renames min and max macros to qs_min and qs_max since min and
+//   max might already be defined elsewhere.
+//
+// Web page:
+// https://github.com/elgw/quickselect/
+
+
+#include <stddef.h>
+
+// Use to turn on expensive internal debugging
+// #define QUICKSELECT_DEBUG
+
+
+// A quickselect routine
+//
+// if XS is a sorted copy of the array X (smallest first)
+// then
+// qselect_f64(X, nX, s) == XS[s]
+//
+// Arguments:
+//
+//  X the data points
+//  nX number of elements of X
+//  s what element to select from sorted X
+//
+// If nX == 0, or X==NULL the function will return 0
 double
 qselect_f64(const double * X, size_t nX, size_t s);
 
